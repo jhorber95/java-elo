8945c49f922aa5ea7aa257b5d324e7a1a8e230a9
jhorkman bernal <jhorber95@gmail.com>
2019-09-04 14:58:07 -0500

    add description to intelligences

---
+++ src/main/webapp/src/app/test/content/resultado/data.ts
@@ -0,0 +1,10 @@
+export const descriptions = {
+  linguistica: 'Habilidad para utilizar con un dominio avanzado el lenguaje oral y escrito, así como para responder a él. Por ejemplo los políticos u oradores de prestigio.',
+  matematica: 'Habilidad para el razonamiento complejo, la relación causa-efecto, la abstracción y la resolución de problemas. Por ejemplo un campeón de ajedrez o científicos.',
+  VisualEspacial: 'Capacidad de percibir el mundo y poder crear imágenes mentales a partir de la experiencia visual. Por ejemplo fotografía, video, creatividad 3D, etc',
+  cinestesica: 'Corporal: Habilidad de utilizar el cuerpo para aprender y para expresar ideas y sentimientos. Incluye el dominio de habilidades físicas como el equilibrio, la fuerza, la flexibilidad y la velocidad. Ejemplo: Danza o gimnasia.',
+  Musical: 'Habilidad de saber utilizar y responder a los diferentes elementos musicales (ritmo, timbre y tono). Por ejemplo los músicos.',
+  Intrapersonal: 'Habilidad de comprenderse a sí mismo y utilizar este conocimiento para operar de manera efectiva en la vida. Por ejemplo los psícologos y filósofos .',
+  Interpersonal: 'Habilidad de interactuar y comprender a las personas y sus relaciones. Por ejemplo las personas solidarias, miembros de ONGs, campañas de ayuda a refugiados, etc',
+  Ambiental: 'Habilidad para el pensamiento científico, para observar la naturaleza, identificar patrones y utilizarla de manera productiva. Por ejemplo científicos volcados en el naturalismo y la defensa de la naturaleza'
+};
