b4e85476061b452f0c09aaf93b17af94b844a61a
jhorkman bernal <jhorber95@gmail.com>
2019-10-19 14:27:43 -0500

    add db changelog

---
+++ src/main/resources/databasechangelog.sql
@@ -0,0 +1,23 @@
+-- --------------------------------------------
+-- Jhorkman Bernal 07/10/2019
+-- --------------------------------------------
+
+DROP TABLE public.test_history;
+
+CREATE TABLE public.test_history
+(
+    id bigserial,
+    id_fase_item integer,
+    id_user bigint,
+    created_at timestamp without time zone,
+    CONSTRAINT test_history_pkey PRIMARY KEY (id),
+    CONSTRAINT test_history_id_fase_item_fkey FOREIGN KEY (id_fase_item)
+        REFERENCES public.fase_item (fit_id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT test_history_id_user_fkey FOREIGN KEY (id_user)
+        REFERENCES public.usuario (usu_id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+    WITH (
+        OIDS=FALSE
+    );
