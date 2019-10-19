fc2f5dc94f6b0868efd4ed62e0fe3f405f368382
jhorkman bernal <jhorber95@gmail.com>
2019-10-19 15:51:41 -0500

    update table test_history

--- src/main/resources/databasechangelog.sql
+++ src/main/resources/databasechangelog.sql
@@ -9,7 +9,7 @@ CREATE TABLE public.test_history
     id bigserial,
     id_fase_item integer,
     id_user bigint,
-    created_at timestamp without time zone,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT test_history_pkey PRIMARY KEY (id),
     CONSTRAINT test_history_id_fase_item_fkey FOREIGN KEY (id_fase_item)
         REFERENCES public.fase_item (fit_id) MATCH SIMPLE
