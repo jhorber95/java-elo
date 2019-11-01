0f9ff2fbfe8f90b53d369fd502fae113354bc54c
jhorkman bernal <jhorber95@gmail.com>
2019-11-01 15:52:15 -0500

    update test result and save test user when the user is logged

--- src/main/webapp/src/app/test/content/resultado/resultado.component.ts
+++ src/main/webapp/src/app/test/content/resultado/resultado.component.ts
@@ -1,19 +1,20 @@
 import {Component, OnInit, AfterViewInit, Input, OnDestroy} from '@angular/core';
 import {ActivatedRoute, NavigationEnd, Params, Router} from '@angular/router';
 
-import { FormDataTestService } from '../../../services/form-data-test.service';
+import {FormDataTestService} from '../../../services/form-data-test.service';
 import {WorkflowtestService} from '../../../shared/workflowtest/workflowtest.service';
 import {STEPS} from '../../../shared/workflowtest/workflow.model';
 import {ResultData} from '../../../models/test/result-data';
 
 // wowngx
-import { NgwWowService } from 'ngx-wow';
+import {NgwWowService} from 'ngx-wow';
 import 'rxjs/add/operator/filter';
-import { Subscription }   from 'rxjs/Subscription';
+import {Subscription} from 'rxjs/Subscription';
 import {AuthService} from '../../../shared/guard/auth.service';
 
 // sweat alert
 import swal from 'sweetalert';
+import {TestHistoryService} from '../../../admin/content/test-history/test-history.service';
 
 @Component({
   selector: 'app-resultado',
@@ -37,6 +38,7 @@ export class ResultadoComponent implements OnInit, AfterViewInit, OnDestroy {
               private _route: ActivatedRoute,
               private formDataTestService: FormDataTestService,
               private workFlowTestService: WorkflowtestService,
+              private testService: TestHistoryService,
               private authService: AuthService) {
     // wowngx
     this.router.events.filter(event => event instanceof NavigationEnd).subscribe(event => {
@@ -46,7 +48,6 @@ export class ResultadoComponent implements OnInit, AfterViewInit, OnDestroy {
     });
     // wowngx
   }
-
 
 
   ngOnInit() {
@@ -69,72 +70,87 @@ export class ResultadoComponent implements OnInit, AfterViewInit, OnDestroy {
   ngAfterViewInit() {
     window.scrollTo(0, 0);
 
-        $(function () {
-            $('.preloader').fadeOut();
-        });
+    $(function () {
+      $('.preloader').fadeOut();
+    });
   }
 
   NextFase() {
-      console.log(this.resultTest);
-      this.workFlowTestService.validateStep(STEPS.resultado);
-      this.workFlowTestService.resetSteps();
+    console.log(this.resultTest);
+    this.workFlowTestService.validateStep(STEPS.resultado);
+    this.workFlowTestService.resetSteps();
 
-      console.log('get ofertas guia vocacional');
+    console.log('get ofertas guia vocacional');
 
-      this.formDataTestService.getOfertasTest(this.resultTest).subscribe(
-        response => {
-          if (response.codigo === 200) {
-            // agregamos el resultado del test al sessionStore
-            sessionStorage.setItem('testVocacional', JSON.stringify(response.data));
-            // verificamos si esta logeado
-            if (localStorage.getItem('token') && this.authService.isAuthenticated()) {
-              // redirigir
-              this.router.navigate(['/ofertas/guiavocacional']);
-            } else {
-              // modal para registrar o logear
-              // tslint:disable-next-line:max-line-length
-              swal('Buen Trabajo', 'Ya casi terminas, solo debes registrarte o iniciar sesión para poder ver el resultado de tu test vocacional.', 'info', {
-                buttons: {
-                  registro: {
-                    text: 'Registrarme',
-                    value: 'registro',
-                  },
-                  login: {
-                    text: 'Iniciar Sesión',
-                    value: 'login',
-                  },
+    this.formDataTestService.getOfertasTest(this.resultTest).subscribe(
+      response => {
+        if (response.codigo === 200) {
+          // agregamos el resultado del test al sessionStore
+          sessionStorage.setItem('testVocacional', JSON.stringify(response.data));
+          // verificamos si esta logeado
+          if (localStorage.getItem('token') && this.authService.isAuthenticated()) {
+            // redirigir
+
+            const _test = this.formDataTestService.getFormDataTestSubmit();
+            const _user = JSON.parse(localStorage.getItem('user'));
+
+            const entity: any = {
+              user: _user,
+              test: _test
+            };
+            // debugger;
+            this.testService.sendUserTest(entity).subscribe(
+              res => {
+                console.log(res)
+              }, error1 => console.log(error1)
+            );
+
+            this.router.navigate(['/ofertas/guiavocacional']);
+          } else {
+            // modal para registrar o logear
+            // tslint:disable-next-line:max-line-length
+            swal('Buen Trabajo', 'Ya casi terminas, solo debes registrarte o iniciar sesión para poder ver el resultado de tu test vocacional.', 'info', {
+              buttons: {
+                registro: {
+                  text: 'Registrarme',
+                  value: 'registro',
                 },
-              })
-                .then((value) => {
-                  switch (value) {
+                login: {
+                  text: 'Iniciar Sesión',
+                  value: 'login',
+                },
+              },
+            })
+              .then((value) => {
+                switch (value) {
 
-                    case 'registro':
-                      sessionStorage.setItem('redirect-test', 'true');
-                      // redirigir
-                      this.router.navigate(['/registrate']);
-                      break;
+                  case 'registro':
+                    sessionStorage.setItem('redirect-test', 'true');
+                    // redirigir
+                    this.router.navigate(['/registrate']);
+                    break;
 
-                    case 'login':
-                      sessionStorage.setItem('redirect-test', 'true');
-                      // redirigir
-                      this.router.navigate(['/login']);
-                      break;
+                  case 'login':
+                    sessionStorage.setItem('redirect-test', 'true');
+                    // redirigir
+                    this.router.navigate(['/login']);
+                    break;
 
-                  }
-                });
-            }
-
-          }else {
-            console.log('No se pudieron obtener las ofertas del test vocacional');
+                }
+              });
           }
-        },
-        error => {
-          console.log(<any>error);
+
+        } else {
+          console.log('No se pudieron obtener las ofertas del test vocacional');
         }
-      );
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
 
 
-      this.error = 'Error, seleccione mínimo 2 y máximo 4 imagenes por fase';
+    this.error = 'Error, seleccione mínimo 2 y máximo 4 imagenes por fase';
 
   }
 
