c8a529a710e1010345a13168933f4514f6072bdd
jhorkman bernal <jhorber95@gmail.com>
2019-11-04 11:22:12 -0500

    create new tables

--- src/main/resources/databasechangelog.sql
+++ src/main/resources/databasechangelog.sql
@@ -21,3 +21,53 @@ CREATE TABLE public.test_history
     WITH (
         OIDS=FALSE
     );
+
+
+-- --------------------------------------------
+-- Jhorkman Bernal 20/10/2019
+-- --------------------------------------------
+
+DROP TABLE public.noticia;
+
+CREATE TABLE public.noticia
+(
+    id bigserial NOT NULL ,
+    titulo character varying(250),
+    descripcion character varying(150),
+    imagen_pricipal character varying(300),
+    contenido text,
+    created_at timestamp without time zone,
+    autor bigint,
+    CONSTRAINT noticia_pkey PRIMARY KEY (id),
+    CONSTRAINT noticia_autor_fkey FOREIGN KEY (autor)
+        REFERENCES public.usuario (usu_id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+    WITH (
+        OIDS=FALSE
+    );
+
+DROP TABLE public.comentario;
+
+CREATE TABLE public.comentario
+(
+    id bigserial NOT NULL,
+    noticia_id bigint,
+    usuario_id bigint,
+    comentario text,
+    fecha timestamp without time zone,
+    padre bigint,
+    CONSTRAINT comentario_pkey PRIMARY KEY (id),
+    CONSTRAINT comentario_noticia_id_fkey FOREIGN KEY (noticia_id)
+        REFERENCES public.noticia (id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT comentario_padre_fkey FOREIGN KEY (padre)
+        REFERENCES public.comentario (id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT comentario_usuario_id_fkey FOREIGN KEY (usuario_id)
+        REFERENCES public.usuario (usu_id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+    WITH (
+        OIDS=FALSE
+    );
